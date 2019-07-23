68e9e558b82652f87b73a29b86d18764c91b3ef4
Arran Jacques <arranjacques@gmail.com>
2019-07-23 15:43:18 +0100

    Remove support logic and styles.

--- src/foundation/custom.d.ts
+++ src/foundation/custom.d.ts
@@ -2,13 +2,3 @@ declare module '*.svg' {
     const content: any;
     export default content;
 }
-
-type BemModifiers = string | string[]
-
-interface ClassName {
-    className?: string
-}
-
-interface LooseObject {
-    [key: string]: any
-}
